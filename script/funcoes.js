7096844b75dcdac7d41ad865d4b9c044b19f5e56
rs-rocha <robsonrocha.unb@gmail.com>
2026-06-30 21:14:22 -0300

    atribui as tarefas para implementar

--- script/funcoes.js
+++ script/funcoes.js
@@ -9,6 +9,7 @@ Tratamento de Exceções: Captura de erros de requisição caso o
 título não seja encontrado ou a chave da API atinja o limite. 
 Tratar o retorno vazio de forma graciosa.
 
+patrick
 Formulários: Sistema de avaliação do filme selecionado, onde o usuário 
 insere uma nota (1 a 5 estrelas) e escreve uma crítica/resenha.
 
@@ -18,6 +19,7 @@ resenhas e notas estruturadas no localStorage.
 Lógica de Programação: Verificação para impedir a duplicação do mesmo 
 filme na lista de favoritos e ordenação dos filmes por nota mais alta.
 
+patrick
 Estrutura de Páginas: 
 1.Página de Busca (Pesquisa e exibição de resultados) e 
 2.Página de Avaliações (Exibição do catálogo pessoal do usuário).
