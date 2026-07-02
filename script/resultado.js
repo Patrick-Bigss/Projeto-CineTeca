3b99def599a17a61703265ecb4a8f744080aaa38
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-02 19:46:24 -0300

    Muda a imagem do filme depois dos dasdos

--- script/resultado.js
+++ script/resultado.js
@@ -9,11 +9,12 @@ function resultado(titulo, ano, escritor, poster){
 
     //alert(console.log(localStorage.key(i-1).nome));
 
-    inserirImagem(poster);    
     cardFilme.innerHTML = 
-        "<p><strong>Título:</strong> " + titulo + "</p>" +
-        "<p><strong>Ano:</strong> " + ano + "</p>" +
-        "<p><strong>Escritor:</strong> " + escritor + "</p>";
-    
+    "<p><strong>Título:</strong> " + titulo + "</p>" +
+    "<p><strong>Ano:</strong> " + ano + "</p>" +
+    "<p><strong>Escritor:</strong> " + escritor + "</p>";
+    //"<div id='imagem'></div>"
     sessaoLista.appendChild(cardFilme);
+
+    inserirImagem(poster);    
 }
