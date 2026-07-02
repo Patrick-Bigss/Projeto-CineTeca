417bd4f433203bd13f465cfe89ec72e90ef5535f
rs-rocha <robsonrocha.unb@gmail.com>
2026-07-01 21:38:43 -0300

    Implementa o tratamento de erro qdo a busca retorna titulo undefined

---
+++ script/tratarErro.js
@@ -0,0 +1,29 @@
+function tratarErroBusca(titulo, ano, escritor){
+    /*
+    Verifica se o título na busca está undefined, outros erros relacioandos
+    a busca podem ser incorporados aqui
+    */
+     try {
+        if(titulo===undefined){
+            alert("título nao encontrado")
+            throw new Error("Esse filme nao existe!")
+        }else{
+            resultado(titulo, ano, escritor);            
+        }
+        
+    } catch (error) {
+        console.error("Erro encontrado: " + error.message);
+    }
+}
+
+function tratarListaDuplicada(){
+    /* 
+    Verifica se algum filme da lista do usuario já foi selecionado
+    */
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
