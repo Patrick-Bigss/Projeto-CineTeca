48763cbda312b2e5ffea76016b1e54b30fd19863
Patrick Leal <Patrick942009@hotmail.com>
2026-07-06 21:17:01 +0000

    finalização da pagina de avaliação e outros retoques

--- CineTeca/JS/Avaliacoes.js
+++ CineTeca/JS/Avaliacoes.js
@@ -1,14 +1,14 @@
- let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+// Recupera os filmes salvos
+let favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
 
+// Mostra os filmes na tela
 mostrarAvaliacoes();
 
 function mostrarAvaliacoes() {
 
     let lista = document.getElementById("listaAvaliacoes");
 
-console.log(lista);
-
-    let lista = document.getElementById("listaAvaliacoes");
+    lista.innerHTML = "";
 
     if (favoritos.length == 0) {
 
@@ -18,50 +18,106 @@ console.log(lista);
 
     }
 
-    lista.innerHTML = `
+    for (let i = 0; i < favoritos.length; i++) {
 
-        <div class="card">
+        // Se ainda não existir nota, deixa como 1
+        if (favoritos[i].nota == undefined) {
 
-            <div class="card-body">
+            favoritos[i].nota = "1";
 
-                <h4>${favoritos[0].titulo}</h4>
+        }
 
-                <p>${favoritos[0].ano}</p>
+        lista.innerHTML +=
 
-                <p>${favoritos[0].diretor}</p>
+        "<div class='card mb-3'>" +
 
-                <img src="${favoritos[0].poster}" width="200">
+            "<div class='card-body'>" +
 
-                <br><br>
+                "<h4>" + favoritos[i].titulo + "</h4>" +
 
-                <button
-                    class="btn btn-warning"
-                    onclick="editarAvaliacao()"> Editar</button>
+                "<p>" + favoritos[i].ano + "</p>" +
 
-                <button
-                    class="btn btn-danger" onclick="removerAvaliacao()"> Excluir </button>
+                "<p>" + favoritos[i].diretor + "</p>" +
 
-            </div>
+                "<img src='" + favoritos[i].poster + "' width='200'>" +
 
-        </div>
+                "<br><br>" +
 
-    `;
+                "<label><strong>Sua nota:</strong></label>" +
+
+                "<select id='nota" + i + "' class='form-select'>" +
+
+                    "<option value='1'" + (favoritos[i].nota == "1" ? " selected" : "") + ">⭐ 1</option>" +
+
+                    "<option value='2'" + (favoritos[i].nota == "2" ? " selected" : "") + ">⭐⭐ 2</option>" +
+
+                    "<option value='3'" + (favoritos[i].nota == "3" ? " selected" : "") + ">⭐⭐⭐ 3</option>" +
+
+                    "<option value='4'" + (favoritos[i].nota == "4" ? " selected" : "") + ">⭐⭐⭐⭐ 4</option>" +
+
+                    "<option value='5'" + (favoritos[i].nota == "5" ? " selected" : "") + ">⭐⭐⭐⭐⭐ 5</option>" +
+
+                "</select>" +
+
+                "<br>" +
+
+                "<button class='btn btn-success' onclick='salvarNota(" + i + ")'>" +
+                "Salvar Nota" +
+                "</button>" +
+
+                "<br><br>" +
+
+                "<button class='btn btn-warning' onclick='editarAvaliacao(" + i + ")'>" +
+                "Editar" +
+                "</button> " +
+
+                "<button class='btn btn-danger' onclick='removerAvaliacao(" + i + ")'>" +
+                "Excluir" +
+                "</button>" +
+
+            "</div>" +
+
+        "</div>";
+
+    }
 
 }
 
-function removerAvaliacao(indice){
+function salvarNota(indice) {
 
-    favoritos.splice(indice,1);
+    favoritos[indice].nota =
+    document.getElementById("nota" + indice).value;
 
-    localStorage.setItem("favoritos", JSON.stringify(favoritos));
+    localStorage.setItem(
+        "favoritos",
+        JSON.stringify(favoritos)
+    );
 
-    mostrarAvaliacoes();
+    alert("Nota salva com sucesso!");
 
 }
 
-function editarAvaliacao(indice){
+function removerAvaliacao(indice) {
 
-    alert("Editar: " + favoritos[indice].titulo);
+    let confirmar = confirm("Deseja remover este filme?");
 
+    if (confirmar) {
+
+        favoritos.splice(indice, 1);
+
+        localStorage.setItem(
+            "favoritos",
+            JSON.stringify(favoritos)
+        );
+
+        mostrarAvaliacoes();
+
+    }
+
+}
+
+function editarAvaliacao(indice) {
+
+    alert("Editar avaliação do filme: " + favoritos[indice].titulo);
 
 }
